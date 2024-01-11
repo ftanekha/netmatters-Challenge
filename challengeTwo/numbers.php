8e5007ac0e0dbb18a23eeaf7a92cae6780060722
ftanekha <farai.tanekha@gmail.com>
2024-01-11 12:22:56 +0000

    Create a PHP program that takes input of 4 numeric values via a console program - adds the values together and outputs the answer - subtracts the values and outputs the answer - multiplies the values and outputs the answer - divides the values and outputs the answer - combination of the above and outputs the answer - Answers should be displayed on a new line

---
+++ challengeTwo/numbers.php
@@ -0,0 +1,43 @@
+<?php
+
+function getUserInput(){
+    #get user-input via console
+    echo "Please enter 4 whole numbers, (integers) seperated by a comma, below:";
+    echo "\n";
+    #get user input as string
+    $user_input = readline("e.g 1, 2, 4... >>>  ");
+    echo "\n";
+    echo $user_input;
+    echo "\n";
+    #convert user-input strings to integers
+    $user_input_string_array = explode(',', $user_input);
+    $user_input_int_array = [
+        $user_input_string_array[0], $user_input_string_array[1], $user_input_string_array[2], $user_input_string_array[3]
+    ];
+    return $user_input_int_array;
+}
+#verify that getUserInput() returns user input
+$user_input = getUserInput();
+#print_r($user_input);
+#echo implode(', ', $user_input);
+#echo "\n";
+//part A: add the values together
+$sum_of_user_input = $user_input[0] + $user_input[1] + $user_input[2] + $user_input[3];
+#output the answer
+echo "The sum of the integers you entered is " . $sum_of_user_input . ".";
+echo "\n";
+//part B: subtract the values together (negate the sum)
+$diff_of_user_input = 0 - $sum_of_user_input;
+#output the answer
+echo "The resulting difference of the integers you entered is " . $diff_of_user_input . ".";
+echo "\n";
+//part C: multiply the values together
+$product_of_user_input = $user_input[0] * $user_input[1] * $user_input[2] * $user_input[3];
+#output the answer
+echo "The product of the integers you entered is " . $product_of_user_input . ".";
+echo "\n";
+//part D: divide the values together
+$quotient_of_user_input = 1 / $product_of_user_input;
+#output the answer
+echo "The quotient of the integers you entered is " . $quotient_of_user_input . ".";
+echo "\n";
